0faa28e0a410f97e8657704df7a5da5ed6137911
José Nieto <jdnv.123@gmail.com>
2018-11-01 23:22:10 -0500

    Market circular button

--- src/scenes/Home/components/Compra/index.js
+++ src/scenes/Home/components/Compra/index.js
@@ -7,6 +7,9 @@ export default class Compra extends React.Component {
     render() {
         return (
             <div className="section">
+                    <div id="toMarket">
+                        <span>MARKET</span>
+                    </div>
                 <ProductBanner />
                 <div id="compraHeader">#COMPRA</div>
             </div>
