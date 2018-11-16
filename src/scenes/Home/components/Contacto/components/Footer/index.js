de8fdf86ef9785730bff82a2fe3f6cf5367be2c3
José Nieto <jdnv.123@gmail.com>
2018-11-15 23:01:24 -0500

    Footer styling, pt. 2

--- src/scenes/Home/components/Contacto/components/Footer/index.js
+++ src/scenes/Home/components/Contacto/components/Footer/index.js
@@ -7,26 +7,27 @@ export default class Footer extends React.Component {
         return (
             <div className="footer wrapper">
                 <div id="footer">
-                    <div className="ui three column grid">
-                        <div className="first column">
-                        <img src="images/atarrayaLogo.svg" alt="images/atarrayaLogo.png" />
+                    <div className="ui grid">
+                        <div className="first four wide column">
+                            <img src="images/logo.png" alt="Atarraya logo" />
                         </div>
-                        <div className="second column">
+                        <div className="second eight wide column">
                             <div className="ui two column grid">
                                 <div className="column">
-                                    <span>Home</span><br/>
-                                    <span>Market</span><br/>
-                                    <span>Lifestyle</span>
+                                    <a href="/">Home</a><br/>
+                                    <a href="/market">Market</a><br/>
+                                    <a href="/lifestyle">Lifestyle</a>
                                 </div>
                                 <div className="column">
-                                    <span>Nosotros</span><br/>
-                                    <span>Contacto</span><br/>
-                                    <span>FAQ</span>
+                                    <a href="/nosotros">Nosotros</a><br/>
+                                    <a href="/contacto">Contacto</a><br/>
+                                    <a href="/faq">FAQ</a>
                                 </div>
                             </div>
                         </div> 
-                        <div className="third column">
-                            <span>#SIGUENOS</span>
+                        <div className="third four wide column">
+                            <a>#SIGUENOS</a>
+                            <i className="huge instagram icon"></i><i className="huge facebook icon"></i>
                         </div>
                     </div>              
                 </div>
